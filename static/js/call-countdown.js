cb657b5215677d03fee1af18df7d97f6d48a0c84
Ksiusha5 <ksiusha5@mail.ru>
2014-02-28 22:17:04 +0200

    My fixes

---
+++ static/js/call-countdown.js
@@ -0,0 +1,7 @@
+var myCountdown1 = new Countdown({
+									time: 86400 * 4, // 86400 seconds = 1 day
+									width: 170,
+									height: 60,
+									rangeHi: "day",
+									style: "flip"
+								});
